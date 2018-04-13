96945f90a8e99b76cd4cab2069873f3a90816f8c
filipao223 <jpmontenegro67@gmail.com>
2018-04-13 15:50:31 +0100

    Pasta só com as classes

---
+++ Classes/Cliente.java
@@ -0,0 +1,10 @@
+// Classe que representa um cliente
+// Como são indistintos neste exemplo, está vazia
+
+class Cliente {
+    boolean geral;
+
+    Cliente(boolean tipo){
+        this.geral = tipo;
+    }
+}
